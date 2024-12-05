38de0a0fd6031e9a1a1b2649e33d52fa25667047
abel-72 <abelnathanjames@gmail.com>
2024-12-05 15:14:33 +0000

    Create BMI histogram

---
+++ Health_Vis.R
@@ -0,0 +1,21 @@
+install.packages("readr")
+
+install.packages("GGally")
+install.packages("NHANES")
+
+library(tidyverse)
+library(GGally)
+library(readr)
+library(NHANES)
+
+data("NHANES")
+View(NHANES)
+
+##create a subtable that only has BMI, age, gender and cholesterol
+
+health_data <- NHANES %>% select(BMI,Age, Gender, TotChol)
+
+##create a histogram of BMI using ggplot
+
+ggplot(health_data, aes (x=BMI)) +
+  geom_histogram((bindwith = 1))
